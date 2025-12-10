7cd011014ff27c02f41d67bf9d2c39cf6b24a93e
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2025-12-10 01:44:12 +0100

    fin de dia está mal

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto';  
 import AsyncStorage from '@react-native-async-storage/async-storage';  
 import { createClient, processLock } from '@supabase/supabase-js';  
-import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY } from '@env';  
+import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY } from '@env';
   
 export const supabase = createClient(  
   EXPO_PUBLIC_SUPABASE_URL,  
